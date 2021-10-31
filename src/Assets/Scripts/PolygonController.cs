625d387af1202bc5ade480e445a576909b6f0365
Takashi Imagire <imagire@gmail.com>
2021-11-01 00:47:43 +0900

    add Polygone Scene

---
+++ src/Assets/Scripts/PolygonController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PolygonController : MonoBehaviour
+{
+    private Mesh mesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+
+        Vector3[] vertices = {
+            new Vector3(1.0f, 0.0f, 0.0f),
+            new Vector3(0.0f, 1.0f, 0.0f),
+            new Vector3(0.0f, 0.0f, 1.0f),
+        };
+        Color[] colors = { Color.red, Color.green, Color.blue};
+        int[] triangles = {0, 1, 2};
+
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+        mesh.colors = colors;
+        mesh.triangles = triangles;
+
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+    }
+}
+
